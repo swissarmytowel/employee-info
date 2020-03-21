4e2e0801f94353b641b3671bc34b703161e3c6fe
swissarmytowel <swissarmytowel@gmail.com>
2020-03-21 18:08:58 +0300

    Fix links & add signup page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,4 +4,7 @@ class StaticPagesController < ApplicationController
 
   def login
   end
+
+  def signup
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  get '/signup', to: 'users#new'
   get '/login', to: 'static_pages#login'
   root 'static_pages#home'
 end
