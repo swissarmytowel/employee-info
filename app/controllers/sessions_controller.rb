f24730fdcbbc39bcb70fbd3401e4a90797762a07
swissarmytowel <swissarmytowel@gmail.com>
2020-03-22 20:45:47 +0300

    Add basic log in find and authenticate logic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,10 +2,17 @@ class SessionsController < ApplicationController
   def new
   end
 
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+    else
+      flash.now[:danger] = 'Ivalid email and/or password'
+      render 'new'
+    end
+  end
+
   def destroy
   end
 
-  def create
-  end
-  
+
 end
