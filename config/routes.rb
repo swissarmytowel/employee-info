dde138c5a4ae34f81535bfc88d44ab374e2ce856
swissarmytowel <swissarmytowel@gmail.com>
2020-03-22 20:26:26 +0300

    Add sessions controller & a basic login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
+
   get '/signup', to: 'users#new'
-  get '/login', to: 'static_pages#login'
+  get '/login',  to: 'sessions#new'
+  get '/logout', to: 'sessions#destroy'
+
+  post '/login',  to: 'sessions#create'
+
   resources :users
 end
