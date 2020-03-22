89d4ba7acb82f131d400c9115c7710001ff3f16a
swissarmytowel <swissarmytowel@gmail.com>
2020-03-22 21:33:14 +0300

    Add session helper & login/logout links

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,15 @@
 module SessionsHelper
+    def log_in(user)
+        session[:user_id] = user.id
+    end
+
+    def get_current_user
+        if session[:user_id]
+            @current_user ||= User.find_by(id: session["user_id"])
+        end
+    end
+
+    def is_logged_in?
+        return !get_current_user.nil?
+    end
 end
