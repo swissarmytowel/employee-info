3e848dd828a1c3bfab401f09f35693aba2992482
swissarmytowel <swissarmytowel@gmail.com>
2020-03-22 18:32:35 +0300

    Add initial users show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
   get '/signup', to: 'users#new'
   get '/login', to: 'static_pages#login'
-  root 'static_pages#home'
+  resources :users
 end
