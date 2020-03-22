1790d06a4fdfcc2e23bb93f05726b6608e24f795
swissarmytowel <swissarmytowel@gmail.com>
2020-03-22 17:03:40 +0300

    Add email index & downcase it before save

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    before_save {self.email = email.downcase}
     validates :name, presence: true, uniqueness: { case_sensitive: true },
             length: {maximum: 50}
             
