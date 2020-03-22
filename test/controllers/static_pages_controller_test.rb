63eb9799e9a8f0d93b009e026519f77d4ae1c4db
swissarmytowel <swissarmytowel@gmail.com>
2020-03-22 23:38:06 +0300

    Fix bcrypt version and fix tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,13 +2,7 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get static_pages_home_url
+    get root_url
     assert_response :success
   end
-
-  test "should get help" do
-    get static_pages_help_url
-    assert_response :success
-  end
-
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,49 +2,49 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: "John Doe", email: "jdoe@gmail.com", 
-    password: "aaaaa", password_confirmation: "aaaaa")
+    @test_user_instance = User.new(name: "John Doe", email: "jdoe@gmail.com", 
+      password: "a" * 7, password_confirmation: "a" * 7)
   end
 
   test "Initial instance should be valid" do
-      assert @user.valid?
+      assert @test_user_instance.valid?
    end
 
   test "Name field should be present" do
-     @user.name = ""
-     assert_not @user.valid?
+     @test_user_instance.name = ""
+     assert_not @test_user_instance.valid?
    end
 
   test "Email should be present" do
-      @user.email = ""
-      assert_not @user.valid?
+      @test_user_instance.email = ""
+      assert_not @test_user_instance.valid?
   end
 
   test "Name length should be less than 50 chars" do
-    @user.name = "John" * 50
-    assert_not @user.valid?
+    @test_user_instance.name = "John" * 50
+    assert_not @test_user_instance.valid?
   end
   
   test "Email length should be less than 80 chars" do
-    @user.email = "a" * 80 + "@test.info"
-    assert_not @user.valid?
+    @test_user_instance.email = "a" * 80 + "@test.info"
+    assert_not @test_user_instance.valid?
   end
 
   test "Email should be of right format" do
-    @user.email = "test.com"
-    assert_not @user.valid?
+    @test_user_instance.email = "test.com"
+    assert_not @test_user_instance.valid?
   end
 
   test "Email should be unique" do
-    duplicate = @user.dup
-    @user.save
+    duplicate = @test_user_instance.dup
+    @test_user_instance.save
     assert_not duplicate.valid? 
   end
 
   test "Emails should be saved in lowercase" do
     mixed_case_email = "TeStemail@GmaIl.COM"
-    @user.email = mixed_case_email
-    @user.save
-    assert_equal mixed_case_email.downcase, @user.reload.email
+    @test_user_instance.email = mixed_case_email
+    @test_user_instance.save
+    assert_equal mixed_case_email.downcase, @test_user_instance.reload.email
   end
 end
