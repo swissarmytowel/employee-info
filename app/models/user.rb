04aa580efce718865d5cbf999dea6ee1802186be
swissarmytowel <swissarmytowel@gmail.com>
2020-03-22 17:45:52 +0300

    Add password to user & add encrypting gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
             length: {maximum: 50}
             
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    validates :email, presence: true, uniqueness: { case_sensitive: false }, 
+    validates :email, presence: true, uniqueness: true, 
             length: {maximum: 80}, format: { with: VALID_EMAIL_REGEX }
+
+    validates :password_digest, presence: true,  length: {minimum: 6}
+    has_secure_password
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: "John Doe", email: "jdoe@gmail.com")
+    @user = User.new(name: "John Doe", email: "jdoe@gmail.com", 
+    password: "aaaaa", password_confirmation: "aaaaa")
   end
 
   test "Initial instance should be valid" do
@@ -23,7 +24,7 @@ class UserTest < ActiveSupport::TestCase
     @user.name = "John" * 50
     assert_not @user.valid?
   end
-
+  
   test "Email length should be less than 80 chars" do
     @user.email = "a" * 80 + "@test.info"
     assert_not @user.valid?
@@ -38,5 +39,12 @@ class UserTest < ActiveSupport::TestCase
     duplicate = @user.dup
     @user.save
     assert_not duplicate.valid? 
+  end
+
+  test "Emails should be saved in lowercase" do
+    mixed_case_email = "TeStemail@GmaIl.COM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
   end
 end
