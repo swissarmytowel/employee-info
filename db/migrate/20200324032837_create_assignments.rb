337ac7b8b1e933460d66fe3ea942b2f732c14031
swissarmytowel <swissarmytowel@gmail.com>
2020-03-24 19:16:41 +0300

    Return DB seeding script

--- db/migrate/20200324032837_create_assignments.rb
+++ db/migrate/20200324032837_create_assignments.rb
@@ -6,8 +6,5 @@ class CreateAssignments < ActiveRecord::Migration[6.0]
 
       t.timestamps
     end
-    # add_index :assignments, :employee
-    # add_index :assignments, :project
-    # add_index :assignments, [:employee, :project]
   end
 end
